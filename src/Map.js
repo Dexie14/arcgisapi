997571f93e2a5eccaa1f7ce8780dcccde55a4301
Adeoniye Adelu <adeluadeoniye14@gmail.com>
2022-08-01 10:35:23 +0100

    arcgis

---
+++ src/Map.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { useRef, useEffect } from 'react';
+import { loadModules } from "esri-loader";
+
+
+function Map() {
+    const MapEl = useRef(null);
+
+    useEffect(
+        () => {
+            let view;
+            loadModules(["esri/views/MapView", "esri/WebMap", "esri/layers/GeoJSONLayer"],{
+                css: true
+            }).then(([MapView,WebMap,GeoJSONLayer])=>{
+                    const webmap= new WebMap({
+                        basemap:'topo-vector'
+                    })
+                    view= new MapView({
+                        map:webmap,
+                        center:[-83,42],
+                        // center:[3.406448, 6.465422],
+                        zoom:10,
+                        // use ref as container
+                        container:MapEl.current
+                    })
+                    const geojsonLayer= new GeoJSONLayer({
+                        url:"https://raw.githubusercontent.com/adarshvarma15/mygeojson/main/RMS_Crime_Incidents%20edited.geojson",
+                    });
+                    webmap.add(geojsonLayer);
+                })
+
+            return()=>{
+                //close the view
+                if(!!view){
+                    view.destroy()
+                    view=null
+                }
+            }
+            })
+
+  return (
+    <div style={{height:800}} ref={MapEl}>
+
+    </div>
+  )
+}
+
+export default Map;
